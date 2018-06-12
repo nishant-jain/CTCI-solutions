6e58d34dbbed5553823010b666e83465af02df96
Nishant Jain <nishantjain19991@gmail.com>
2018-06-12 22:50:52 +0530

    use single line comments

--- python/arrays_and_strings/1.1.py
+++ python/arrays_and_strings/1.1.py
@@ -2,10 +2,9 @@
 # base solution without any extra mem is to iterate over all characters, and match with every other character, O(n^2)
 
 def uniq_string_no_mem(test_string):
-	'''
-	sort the string first  O(lg n), then check every consecutive element for equality
-	may take up extra space for sorting
-	'''
+	# sort the string first  O(lg n), then check every consecutive element for equality
+	# may take up extra space for sorting
+	
 	temp = sorted(test_string);
 
 	for i in range(len(temp)-1):
@@ -14,10 +13,9 @@ def uniq_string_no_mem(test_string):
 	return "unique"
 
 def uniq_string_extra_mem(test_string):
-	'''
-	iterate over string, maintain a dictionary/list(as it also allows ) of encountered characters, return non-unique if character already exists
-	O(n), although takes O(n) extra space too.
-	'''
+	# iterate over string, maintain a dictionary/list(as it also allows ) of encountered characters, return non-unique if character already exists
+	# O(n), although takes O(n) extra space too.
+
 	char_counter = dict()
 	for i in test_string:
 		if i in char_counter:
